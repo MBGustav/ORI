599bb4cc854b3ac5071b7a3e3b6bf86c93fa35df
Guga <gbarreto@estudante.ufscar.br>
2024-07-23 18:23:15 -0300

    separating .h files

--- Project/ScratchQL/include/FloatHandler.h
+++ Project/ScratchQL/include/FloatHandler.h
@@ -2,6 +2,8 @@
 #define _FLOATHANDLER_H_
 
 #include <string>
+#include <stdexcept>
+#include <fstream>
 #include "DataInterface.h"
 
 // #include "Date.h"
@@ -25,31 +27,5 @@ public:
     virtual void fwrite(std::fstream &file);
     virtual void fread(std::fstream &file);    
 };
-
-FloatHandler::FloatHandler(float float_num): _data(float_num){}
-
-FloatHandler::~FloatHandler(){}
-
-float FloatHandler::get_value(){return _data;}
-
-size_t FloatHandler::bin_size(){return sizeof(float);}
-
-DataType FloatHandler::read_DataType() { return DataType::FLOAT;}
-
-
-void FloatHandler::set_value(float val){this->_data = val;}
-
-string FloatHandler::toString(){ return std::to_string(get_value());}
-
-void FloatHandler::parseString(string &data){set_value(std::stof(data));}
-
-void FloatHandler::fwrite(std::fstream &file){
-    file.write(reinterpret_cast<char*>(this), sizeof(*this));
-}
-
-void FloatHandler::fread(std::fstream &file) {
-    file.read(reinterpret_cast<char*>(this), sizeof(*this));
-}
-
 
 #endif /*_DATAHANDLER_H_*/
--- Project/ScratchQL/include/IntHandler.h
+++ Project/ScratchQL/include/IntHandler.h
@@ -2,7 +2,8 @@
 #define _INTHANDLER_H_
 
 #include "DataInterface.h"
-
+#include <stdexcept>
+#include <fstream>
 
 class IntHandler : public DataInterface
 {
@@ -23,38 +24,5 @@ public:
     virtual void fwrite(std::fstream &file);
     virtual void fread(std::fstream &file);
 };
-
-IntHandler::IntHandler(int int_num): _data(int_num){}
-
-IntHandler::IntHandler(std::fstream &file){fread(file);}
-
-IntHandler::~IntHandler(){}
-
-size_t IntHandler::bin_size(){return sizeof(int);}
-
-int IntHandler::get_value(){return _data;}
-
-DataType IntHandler::read_DataType(){return DataType::INT;}
-
-void IntHandler::set_value(int val){this->_data = val;}
-
-string IntHandler::toString(){ return std::to_string(get_value());}
-
-void IntHandler::parseString(string &data){set_value(stoi(data));}
-
-
-void IntHandler::fwrite(std::fstream &file) {
-    if (!file.is_open()) {
-        throw std::runtime_error("File not open");
-    }
-    file.write(reinterpret_cast<char*>(&_data), sizeof(_data));
-}
-
-void IntHandler::fread(std::fstream &file) {
-    if (!file.is_open()) {
-        throw std::runtime_error("File not open");
-    }
-    file.read(reinterpret_cast<char*>(&_data), sizeof(_data));
-}
 
 #endif /*_INTHANDLER_H_*/
