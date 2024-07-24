f759ef8a960421432bc9331ce84e37fc7b131fd6
Guga <gbarreto@estudante.ufscar.br>
2024-07-24 08:26:32 -0300

    todo: fix this code

--- Project/ScratchQL/test.cpp
+++ Project/ScratchQL/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
-#include "SQLTable.h"
+
 #include <vector>
+#include <utility>
+#include <string>
+#include <map>
+
 #include "SimpleTableHandler.h"
 #include "DataInterface.h"
 #include "FloatHandler.h"
@@ -8,144 +12,143 @@
 #include "StringHandler.h"
 #include "DateHandler.h"
 #include "HeaderHandler.h"
-#include <string>
-#include <map>
+#include "SimpleTableHandler.h"
 
-using namespace std;
-
-bool dbg_create()
-{
-    v_entity v = {
-        {"ID_NOME", INT },
-        {"ID_CLIENTE", INT },
-        {"N_CLIENTE", STRING }, 
-        {"N_USER", STRING },
-        {"CLIENTE_EXISTE", BOOL }, 
-        {"NUMERO_INT", INT},
-        {"NUMERO_FLOAT", FLOAT}    
-    };
-
-
-    SQLTable Table();
-
-    
-}
+// using namespace std;
 
 
 
 int main()
 {
     int decision = 0;
+    string table_name;
+    string name; 
+    int num_cols;
     while(decision != 9){
-        cout << "1 - Create Table\n2 - Insert Row\n3 - Delete Row\n4 - Update Row\n5 - Exit\n";
-        cin >> decision;
+        std::cout << "1 - Create Table\n2 - Insert Row\n3 - Delete Row\n4 - Update Row\n5 - Exit\n";
+        std::cin >> decision;
         switch (decision)
         {
-        case 1:
+        case 1:{
+
             string name;
-            cout << "Which Name?\n";
-            cin >> name;
+            std::cout << "Which Name?\n";
+            std::cin >> name;
             int num_cols;
-            cout << "How many columns?\n";
-            cin >> num_cols;
-            vector<pair<string, DataType>> cols(num_cols);
+            std::cout << "How many columns?\n";
+            std::cin >> num_cols;
+            vector<std::pair<string, DataType>> cols(num_cols);
             for(int i=0; i<num_cols; i++){
                 string col_name;
-                cout << "Column Name?\n";
-                cin >> col_name;
+                std::cout << "Column Name?\n";
+                std::cin >> col_name;
                 int col_type;
-                cout << "Column Type?\n1 - INT\n2 - FLOAT\n3 - STRING\n4 - DATE\n";
-                cin >> col_type;
+                std::cout << "Column Type?\n1 - INT\n2 - FLOAT\n3 - STRING\n4 - DATE\n";
+                std::cin >> col_type;
                 switch (col_type)
                 {
-                case 1:
+                case 1:{
                     cols[i] = make_pair(col_name, DataType::INT);
                     break;
-                case 2:
+                }
+                case 2:{
                     cols[i] = make_pair(col_name, DataType::FLOAT);
                     break;
-                case 3:
+                }
+                case 3:{
                     cols[i] = make_pair(col_name, DataType::STRING);
                     break;
-                case 4:
+                }
+                case 4:{
                     cols[i] = make_pair(col_name, DataType::DATE);
                     break;
+                }
                 default:
-                    cout << "Invalid Type\n";
+                    std::cout << "Invalid Type\n";
                     break;
                 }
             }
-
             break;
-        case 2:
-            cout << "In witch table?\n";
+        }
+        case 2:{
+            
+            std::cout << "In witch table?\n";
             string table_name;
-            cin >> table_name;
-            cout << "How many columns?\n";
+            std::cin >> table_name;
+            std::cout << "How many columns?\n";
             int num_cols;
-            cin >> num_cols;
-            vector<data_t> row(num_cols);
+            std::cin >> num_cols;
+            vector<DataType*> row(num_cols);
             for(int i=0; i<num_cols; i++){
                 string col_name;
-                cout << "Column Name?\n";
-                cin >> col_name;
+                std::cout << "Column Name?\n";
+                std::cin >> col_name;
                 int col_type;
-                cout << "Column Type?\n1 - INT\n2 - FLOAT\n3 - STRING\n4 - DATE\n";
-                cin >> col_type;
+                std::cout << "Column Type?\n1 - INT\n2 - FLOAT\n3 - STRING\n4 - DATE\n";
+                std::cin >> col_type;
                 switch (col_type)
                 {
-                case 1:
+                case 1:{
                     int value;
-                    cout << "Value?\n";
-                    cin >> value;
-                    row[i] = {DataType::INT, value};
+                    std::cout << "Value?\n";
+                    std::cin >> value;
+                    row[i] = dt_alloc(DataType::INT, to_string(value));
                     break;
-                case 2:
+                }
+                case 2:{
                     float value;
-                    cout << "Value?\n";
-                    cin >> value;
-                    row[i] = {DataType::FLOAT, value};
+                    std::cout << "Value?\n";
+                    std::cin >> value;
+                    row[i] = dt_alloc(DataType::FLOAT, to_string(value));
                     break;
-                case 3:
+                }
+                case 3:{
                     string value;
-                    cout << "Value?\n";
-                    cin >> value;
-                    row[i] = {DataType::STRING, value};
+                    std::cout << "Value?\n";
+                    std::cin >> value;
+                    row[i] = dt_alloc(DataType::STRING, (value));
                     break;
-                case 4:
+                }
+                case 4:{
                     string value;
-                    cout << "Value?\n";
-                    cin >> value;
-                    row[i] = {DataType::DATE, value};
+                    std::cout << "Value?\n";
+                    std::cin >> value;
+                    row[i] = dt_alloc(DataType::DATE, value);
                     break;
+                }
                 default:
-                    cout << "Invalid Type\n";
+                    std::cout << "Invalid Type\n";
                     break;
                 }
             }
+        }
             // Table.insertRow(row);
             break;
-        case 3:
-            cout << "In wicht table?\n";
-            cin >> table_name;
-            cout << "Which Row?\n";
+        case 3:{
+
+            std::cout << "In wicht table?\n";
+            std::cin >> table_name;
+            std::cout << "Which Row?\n";
             int row;
-            cin >> row;
+            std::cin >> row;
             // Table.deleteRow(row);
             break;
-        case 4:
-            cout << "In Wicht table?\n";
-            cin >> table_name;
-            cout << "Which Row?\n";
+        }
+        case 4:{
+            std::cout << "In Wicht table?\n";
+            std::cin >> table_name;
+        }
+            std::cout << "Which Row?\n";
             int row;
-            cin >> row;
+            std::cin >> row;
             // Table.updateRow(row);
             break;
-        case 5:
-            cout << "Select Row\n";
+        case 5:{
+            std::cout << "Select Row\n";
             break;
+        }
         default:
-            cout << "Invalid Option\n";
+            std::cout << "Invalid Option\n";
             break;
         }
     }
